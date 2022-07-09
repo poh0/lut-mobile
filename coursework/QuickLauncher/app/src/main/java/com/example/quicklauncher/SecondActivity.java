91b1619f87dd137c1135dfb69b6e1571583c6d6a
Lauri Pöhö <xzeusking@gmail.com>
2022-07-09 17:38:50 +0300

    Create QuickLauncher UI & add SecondActivity

---
+++ coursework/QuickLauncher/app/src/main/java/com/example/quicklauncher/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.example.quicklauncher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        if (getIntent().hasExtra("com.example.SOMETHING")) {
+            TextView tv = (TextView) findViewById(R.id.textView);
+            String text = getIntent().getExtras().getString("com.example.SOMETHING");
+            tv.setText(text);
+        }
+    }
+}
