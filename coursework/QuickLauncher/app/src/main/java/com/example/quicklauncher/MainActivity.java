5371ecf4158133d4ebf121207ebc118b17911013
Lauri Pöhö <xzeusking@gmail.com>
2022-07-11 18:07:40 +0300

    Add functionality to google button

--- coursework/QuickLauncher/app/src/main/java/com/example/quicklauncher/MainActivity.java
+++ coursework/QuickLauncher/app/src/main/java/com/example/quicklauncher/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.quicklauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Attempt to launch an activity within our app
         Button secondActivityBtn = (Button) findViewById(R.id.secondActivityBtn);
         secondActivityBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,6 +23,21 @@ public class MainActivity extends AppCompatActivity {
                 Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class);
                 startIntent.putExtra("com.example.SOMETHING", "HELLO WORLD!");
                 startActivity(startIntent);
+            }
+        });
+
+        // Attempt to launch an activity outside our app
+        Button googleBtn = (Button) findViewById(R.id.googleBtn);
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String google = "http://www.google.com";
+                Uri webaddress = Uri.parse(google);
+
+                Intent gotoGoogle = new Intent(Intent.ACTION_VIEW, webaddress);
+                if (gotoGoogle.resolveActivity(getPackageManager()) != null) {
+                    startActivity(gotoGoogle);
+                }
             }
         });
     }
