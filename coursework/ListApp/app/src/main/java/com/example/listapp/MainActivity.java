2aa0d61ac06f3d9e4d6a59b3a89e2dd2c94f95f1
Lauri Pöhö <xzeusking@gmail.com>
2022-07-12 18:12:14 +0300

    init ListApp & add ListView

---
+++ coursework/ListApp/app/src/main/java/com/example/listapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.listapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView myListView;
+    String[] items;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+        myListView = (ListView) findViewById(R.id.myListView);
+        items = res.getStringArray(R.array.items);
+
+        myListView.setAdapter(new ArrayAdapter<String>(this, R.layout.my_listview_detail, items));
+
+    }
+}
