57052cc6b78c826ce66db9bfbc46e6edf6fb56ec
Lauri Pöhö <xzeusking@gmail.com>
2022-07-16 23:20:13 +0300

    Finish ListApp

--- coursework/ListApp/app/src/main/java/com/example/listapp/MainActivity.java
+++ coursework/ListApp/app/src/main/java/com/example/listapp/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.listapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -27,5 +30,15 @@ public class MainActivity extends AppCompatActivity {
 
         ItemAdapter itemAdapter = new ItemAdapter(this, items, prices, descriptions);
         myListView.setAdapter(itemAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent showDetailActivity =
+                        new Intent(getApplicationContext(), DetailActivity.class);
+                showDetailActivity.putExtra("com.example.ITEM_INDEX", i);
+                startActivity(showDetailActivity);
+            }
+        });
     }
 }
